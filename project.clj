3f17b37bd2b54d10a7562b2aae865ce7d0a5fef9
Daniel Schlaug <daniel@schlaug.com>
2020-07-06 23:05:15 +0200

    Remove rum

--- project.clj
+++ project.clj
@@ -7,7 +7,6 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
                  [org.clojure/test.check "0.10.0-alpha4"]
-                 [rum "0.11.2"]
                  [ysera "1.3.0"]]
 
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
